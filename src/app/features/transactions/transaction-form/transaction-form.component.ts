4af03dcef9acbcfdecf03a7ee1501eb5817596d3
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-03-23 16:57:25 +0800

    added fomantic-ui-css

---
+++ src/app/features/transactions/transaction-form/transaction-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-form',
+  imports: [],
+  templateUrl: './transaction-form.component.html',
+  styleUrl: './transaction-form.component.css',
+  standalone:true
+})
+export class TransactionFormComponent {
+
+}

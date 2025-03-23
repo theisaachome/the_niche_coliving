256dc638e9fb6d5f5f8e3f737d6a50f9115972ff
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-03-23 22:52:07 +0800

    added account form.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {AccountFormComponent} from './features/account/account-form/account-form.component';
+import {TransactionListComponent} from './features/transactions/transaction-list/transaction-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component:TransactionListComponent},
+  {path:'transactions',component:TransactionListComponent},
+    {path:'accounts',component:AccountFormComponent}
+];

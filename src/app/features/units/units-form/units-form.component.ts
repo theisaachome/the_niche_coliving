cdd5db67f68fc09ad1d7000611379df009d7883f
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-03-24 23:05:02 +0800

    units components added.

---
+++ src/app/features/units/units-form/units-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-units-form',
+  imports: [],
+  templateUrl: './units-form.component.html',
+  styleUrl: './units-form.component.css'
+})
+export class UnitsFormComponent {
+
+}
---
+++ src/app/features/units/units-list/units-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-units-list',
+  imports: [],
+  templateUrl: './units-list.component.html',
+  styleUrl: './units-list.component.css'
+})
+export class UnitsListComponent {
+
+}

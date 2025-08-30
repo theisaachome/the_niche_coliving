bc540948b1972434e5b1fc16af4605bb96cd50f2
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-08-30 18:11:54 +0800

    build-backend-api

---
+++ the_niche_api/src/main/java/com/theniche/colivin/TheNicheApiApplication.java
@@ -0,0 +1,13 @@
+package com.theniche.colivin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNicheApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheNicheApiApplication.class, args);
+    }
+
+}
---
+++ the_niche_api/src/test/java/com/theniche/colivin/TheNicheApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theniche.colivin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheNicheApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
